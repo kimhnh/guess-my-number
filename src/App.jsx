102091927107e2ea130bbb779622600627745397
kimhnh <kimhnh@gmail.com>
2023-12-28 17:03:22 +0900

    feat game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,114 @@
+import { useState } from 'react';
 import './index.css';
 
 export default function App() {
+  const randomNum = Math.floor(Math.random() * 20) + 1; // derived state?
+  const [secretNum, setSecretNum] = useState(randomNum);
+  const [guessNum, setGuessNum] = useState('');
+  const [msg, setMsg] = useState('Start guessing...');
+  const [score, setScore] = useState(20);
+  const [highScore, setHighScore] = useState(0);
+  const [hasWon, setHasWon] = useState(false); // change of UI so a derived state would not work
+
+  // Reset all state except highScore
+  function handlePlayAgain() {
+    setSecretNum(randomNum);
+    setGuessNum('');
+    setMsg('Start guessing...');
+    setScore(20);
+    setHasWon(false);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Guard clause, prevent NaN from being input
+    if (!guessNum || guessNum === '') {
+      setMsg('⛔ No number!');
+    }
+
+    // Player wins
+    if (guessNum === secretNum) {
+      setMsg('🎉 Correct Number!');
+      setHasWon(true);
+
+      if (score > highScore) {
+        setHighScore(score);
+      }
+
+      // Player is wrong
+    } else {
+      if (score > 1) {
+        setMsg(guessNum > secretNum ? '📈 Too high!' : '📉 Too low!');
+        setScore((s) => s - 1);
+
+        // Player loses
+      } else {
+        setMsg('💣 You lost the game!');
+        setScore(0);
+      }
+    }
+  }
+
   return (
-    <>
+    <div
+      className="app"
+      style={{ backgroundColor: hasWon && '#60b347' }}
+    >
       <Navbar>
         <Title />
         <Between />
-        <Button>Again!</Button>
-        <Number />
+        <Button
+          className="btn again"
+          onClick={handlePlayAgain}
+        >
+          Again!
+        </Button>
+        <AnswerBlock style={{ width: hasWon && '30rem' }}>{hasWon ? secretNum : '?'}</AnswerBlock>
       </Navbar>
+
       <Main>
-        <LeftBox>
-          <Input />
-          <Button>Check!</Button>
-        </LeftBox>
-        <RightBox>
-          <Message />
-          <Score />
-        </RightBox>
+        <Box className="left">
+          <Form onSubmit={handleSubmit}>
+            <Input
+              guessNum={guessNum}
+              setGuessNum={setGuessNum}
+            />
+            <Button className="btn">Check!</Button>
+          </Form>
+        </Box>
+
+        <Box className="right">
+          <Message msg={msg} />
+          <Score
+            score={score}
+            highScore={highScore}
+          />
+        </Box>
       </Main>
-    </>
+    </div>
   );
 }
 
-function Navbar({ children }) {
-  return <header>{children}</header>;
+// Reusable components
+function Button({ children, className, onClick }) {
+  return (
+    <button
+      className={className}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
 
-function Button({ children }) {
-  return <button className="btn">{children}</button>;
+function Box({ children, className }) {
+  return <section className={className}>{children}</section>;
+}
+
+// Composition
+function Navbar({ children }) {
+  return <header>{children}</header>;
 }
 
 function Between() {
@@ -39,40 +119,45 @@ function Title() {
   return <h1>Guess My Number!</h1>;
 }
 
-function Number() {
-  return <div className="number">?</div>;
+function AnswerBlock({ children, style }) {
+  return (
+    <div
+      className="number"
+      style={style}
+    >
+      {children}
+    </div>
+  );
 }
 
 function Main({ children }) {
   return <main>{children}</main>;
 }
 
-function LeftBox({ children }) {
-  return <section className="left">{children}</section>;
+function Form({ children, onSubmit }) {
+  return <form onSubmit={onSubmit}>{children}</form>;
 }
 
-function Input() {
+function Input({ guessNum, setGuessNum }) {
   return (
     <input
       type="number"
       className="guess"
+      value={guessNum}
+      onChange={(e) => setGuessNum(Number(e.target.value))} // the reason why Number method wasn't working is because I had a component named 'Number'
     />
   );
 }
 
-function RightBox({ children }) {
-  return <section className="right">{children}</section>;
+function Message({ msg }) {
+  return <p className="message">{msg}</p>;
 }
 
-function Message() {
-  return <p className="message">Start guessing...</p>;
-}
-
-function Score() {
+function Score({ score, highScore }) {
   return (
     <>
-      <p className="label-score">💯 Score: 0</p>
-      <p className="label-score">🥇 Highscore: 0</p>
+      <p className="label-score">💯 Score: {score}</p>
+      <p className="label-highscore">🥇 Highscore: {highScore}</p>
     </>
   );
 }
