b64ff7a9bed6e978d1bbc402e55638af6a2d5c2f
kimhnh <kimhnh@gmail.com>
2024-01-01 13:38:31 +0900

    fix handler function and modal item

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 import './index.css';
 
-const modalContent = [
+const modalData = [
   {
-    title: 'Lorem',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, eos!',
-  },
-  {
-    title: 'Ipsum',
+    title: 'Who created Lorem Ipsum?',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore facere recusandae alias quam consectetur dolorum eos. Inventore distinctio eaque sint.',
+      'It is widely believed that the history of Lorem Ipsum originates with Cicero in the 1st Century BC and his text De Finibus bonorum et malorum. This philosophical work, also known as On the Ends of Good and Evil, was split into five books.',
   },
   {
-    title: 'Dolor',
-    description: 'Lorem ipsum dolor sit amet.',
+    title: 'What does it mean?',
+    description:
+      '"Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure".',
+  },
+  {
+    title: 'Lorem Ipsum',
+    description:
+      "Advancing to the 1960s, Lorem Ipsum was made popular by typeface manufacturer Letraset, who used it in their advertising campaigns. Letraset offered pages of Lorem Ipsum as rub down transfer sheets, which were widely used in the pre-computer era for layouts. These transfer pages, known as Letraset Body Type, were featured in the company's advertising and their popular catalogue.",
   },
 ];
 
@@ -21,74 +23,58 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [isActive, setIsActive] = useState(null);
 
-  function handleOpen(num) {
+  function handleClick(num) {
     setIsOpen((o) => !o);
-    setIsActive(num);
-  }
-
-  function handleClose() {
-    setIsOpen((o) => !o);
-    setIsActive(null);
+    setIsActive((s) => (typeof num === 'number' ? num : null));
   }
 
   return (
     <div className="app">
-      <OpenButton
+      <Button
+        num={0}
+        onClick={handleClick}
+      >
+        Marcus Tullius Cicero
+      </Button>
+      <Button
         num={1}
-        onClick={handleOpen}
+        onClick={handleClick}
       >
-        About Lorem
-      </OpenButton>
-      <OpenButton
+        Translation
+      </Button>
+      <Button
         num={2}
-        onClick={handleOpen}
+        onClick={handleClick}
       >
-        About Ipsum
-      </OpenButton>
-      <OpenButton
-        num={3}
-        onClick={handleOpen}
-      >
-        About Dolor
-      </OpenButton>
-      <Modal className={isOpen ? 'modal' : 'modal hidden'}>
-        <CloseButton onClick={handleClose}>&times;</CloseButton>
-        {isActive && (
+        Usage
+      </Button>
+      {isOpen && (
+        <Modal>
+          <Button
+            className="close-modal"
+            onClick={handleClick}
+          >
+            &times;
+          </Button>
           <ModalItem
             key={isActive}
             isActive={isActive}
-            modalContent={modalContent}
+            modalData={modalData}
           />
-        )}
-      </Modal>
-      <Overlay
-        className={isOpen ? 'overlay' : 'overlay hidden'}
-        onClick={handleClose}
-      />
+        </Modal>
+      )}
+      {isOpen && <Overlay onClick={handleClick} />}
     </div>
   );
 }
 
 // Reusable component
-function OpenButton({ children, className = 'show-modal', onClick, num }) {
+function Button({ children, className = 'show-modal', onClick, num }) {
   return (
     <button
       num={num}
       className={className}
-      onClick={() => {
-        onClick(num);
-      }}
-    >
-      {children}
-    </button>
-  );
-}
-
-function CloseButton({ children, onClick }) {
-  return (
-    <button
-      className="close-modal"
-      onClick={onClick}
+      onClick={() => onClick(num)}
     >
       {children}
     </button>
@@ -96,23 +82,23 @@ function CloseButton({ children, onClick }) {
 }
 
 // Composition
-function Modal({ children, className }) {
-  return <div className={className}>{children}</div>;
+function Modal({ children }) {
+  return <div className="modal">{children}</div>;
 }
 
-function ModalItem({ isActive, modalContent }) {
+function ModalItem({ modalData, isActive }) {
   return (
     <>
-      <h1>{modalContent[isActive - 1].title}</h1>
-      <p>{modalContent[isActive - 1].description}</p>
+      <h1>{modalData[isActive].title}</h1>
+      <p>{modalData[isActive].description}</p>
     </>
   );
 }
 
-function Overlay({ className, onClick }) {
+function Overlay({ onClick }) {
   return (
     <div
-      className={className}
+      className="overlay"
       onClick={onClick}
     ></div>
   );
