b638ec479932afea9630f4a6e521a26cac68f01a
kimhnh <kimhnh@gmail.com>
2024-01-12 23:26:07 +0900

    feat re-structure project with new states

--- pig-game/src/App.jsx
+++ pig-game/src/App.jsx
@@ -1,119 +1,130 @@
 import { useState } from 'react';
 import './index.css';
 
+const initialData = [
+  {
+    num: 1,
+    finalScore: 0,
+    currScore: 0,
+    isActive: true,
+    hasWon: false,
+  },
+  {
+    num: 2,
+    finalScore: 0,
+    currScore: 0,
+    isActive: false,
+    hasWon: false,
+  },
+];
+
 export default function App() {
-  const [isActive, setIsActive] = useState(0); // using num prop to indicate active player
-  const p1Playing = isActive === 0; // derived state
-  const [playing, setPlaying] = useState(true);
-  const [diceNum, setDiceNum] = useState(null); // dice image state
-  const [currScoreP1, setCurrScoreP1] = useState(0);
-  const [currScoreP2, setCurrScoreP2] = useState(0);
-  const [scoreP1, setScoreP1] = useState(0);
-  const [scoreP2, setScoreP2] = useState(0);
+  const [gameData, setGameData] = useState(initialData);
+  const [diceIcon, setDiceIcon] = useState(null); // dice icon
+  const [winStatus, setWinStatus] = useState(false);
+  let currScore = gameData.find((g) => g.isActive).currScore; // breaks when isActive is set to false
+  let finalScore = gameData.find((g) => g.isActive).finalScore;
+  let hasWon = gameData.find((g) => g.isActive).hasWon;
+
+  // Update current score (active player ONLY)
+  function handleUpdateCurrScore(current) {
+    setGameData((g) => g.map((i) => (i.isActive ? { ...i, currScore: current } : i)));
+  }
+
+  // Update final score (active player ONLY)
+  function handleUpdateFinalScore(final) {
+    setGameData((g) => g.map((i) => (i.isActive ? { ...i, finalScore: final } : i)));
+  }
+
+  // Switch player
+  function handleSwitchPlayer() {
+    setGameData((g) => g.map((i) => ({ ...i, isActive: !i.isActive })));
+  }
 
   function handleRoll() {
-    const dice = Math.floor(Math.random() * 6 + 1); // 1~6
-    setDiceNum((s) => dice);
-    // check if roll = 1
+    // 1. Generate a number between 1~6
+    const dice = Math.floor(Math.random() * 6 + 1);
+    setDiceIcon(dice); // set "dice icon" to dice number
+
     if (dice !== 1) {
-      // check player
-      if (p1Playing) {
-        setCurrScoreP1((c) => c + dice);
-      } else {
-        setCurrScoreP2((c) => c + dice);
-      }
-      console.log(dice);
-      // wipe current score to 0
+      currScore += dice;
+      handleUpdateCurrScore(currScore);
     } else {
-      // check player
-      if (p1Playing) {
-        setCurrScoreP1(0);
-      } else {
-        setCurrScoreP2(0);
-      }
-      setIsActive((a) => (isActive === 0 ? 1 : 0));
+      handleUpdateCurrScore(0);
+      handleSwitchPlayer();
     }
   }
 
   function handleHold() {
-    if (scoreP1 >= 10 || scoreP2 >= 10) {
-      setPlaying(false); // todo: batch update or update logic not working as intended
+    finalScore += currScore;
+    handleUpdateFinalScore(finalScore); // must be outside if-else statement to prevent handleSwitchPlayer from getting invoked in the else block
+
+    if (finalScore < 10) {
+      handleUpdateCurrScore(0);
+      handleSwitchPlayer();
     } else {
-      if (p1Playing) {
-        setScoreP1((s) => s + currScoreP1);
-        setCurrScoreP1(0);
-      } else {
-        setScoreP2((s) => s + currScoreP2);
-        setCurrScoreP2(0);
-      }
-      setIsActive((a) => (isActive === 0 ? 1 : 0));
+      console.log('you win');
+      setWinStatus(true);
+      setDiceIcon(null);
+      hasWon = true;
+      setGameData((g) => g.map((i) => (i.isActive ? { ...i, hasWon: hasWon } : i)));
     }
   }
 
   function handleNewGame() {
-    setIsActive(0);
-    setPlaying(true);
-    setCurrScoreP1(0);
-    setCurrScoreP2(0);
-    setScoreP1(0);
-    setScoreP2(0);
+    // reset gameData to default values
+    setGameData((g) =>
+      g.map((i) => ({
+        ...i,
+        finalScore: 0,
+        currScore: 0,
+        isActive: i.num === 1 ? true : false,
+        hasWon: false,
+      }))
+    );
+    setWinStatus(false);
   }
 
   return (
     <div className="app">
-      <Player
-        num={0}
-        isActive={isActive}
-        playing={playing}
-      >
-        <Name className="name">player 1</Name>
-        <Score score={scoreP1} />
-        <CurrScoreBox>
-          <CurrLabel />
-          <CurrScore
-            isActive={isActive}
-            currScore={currScoreP1}
-          />
-        </CurrScoreBox>
-      </Player>
-      <Player
-        num={1}
-        isActive={isActive}
-        playing={playing}
-      >
-        <Name className="name">player 2</Name>
-        <Score score={scoreP2} />
-        <CurrScoreBox>
-          <CurrLabel />
-          <CurrScore
-            isActive={isActive}
-            currScore={currScoreP2}
-          />
-        </CurrScoreBox>
-      </Player>
+      {gameData.map((i) => (
+        <Player
+          key={crypto.randomUUID()}
+          isActive={i.isActive}
+          hasWon={i.hasWon}
+        >
+          <Name num={i.num} />
+          <Score score={i.finalScore} />
+          <CurrScoreWrapper>
+            <CurrLabel />
+            <CurrScore currScore={i.currScore} />
+          </CurrScoreWrapper>
+        </Player>
+      ))}
+
       <DiceImage
-        src={`./src/assets/dice-${diceNum}.png`}
-        className={diceNum === null ? 'hidden' : 'dice'}
+        src={diceIcon}
+        className={diceIcon === null ? 'hidden' : 'dice'}
       />
+
+      {/* Buttons */}
       <Button
         className="btn btn--new"
         onClick={handleNewGame}
       >
         🔄 New game
       </Button>
-
       <Button
         className="btn btn--roll"
         onClick={handleRoll}
-        playing={playing}
+        disabled={winStatus}
       >
         🎲 Roll dice
       </Button>
-
       <Button
         className="btn btn--hold"
         onClick={handleHold}
-        playing={playing}
+        disabled={winStatus}
       >
         📥 Hold
       </Button>
@@ -121,40 +132,34 @@ export default function App() {
   );
 }
 
-// Reusable components
-function Button({ className, onClick, children, playing, disabled = 'false' }) {
+// Reusuable components
+function Button({ className, onClick, disabled, children }) {
   return (
     <button
       className={className}
       onClick={onClick}
-      disabled={playing === false}
+      disabled={disabled}
     >
       {children}
     </button>
   );
 }
 
-// note: don't pass down props if they can be resolved in the component
-function Player({ num, isActive, playing, children }) {
-  return (
-    <section
-      num={num}
-      className={isActive === num ? 'player player--active' : 'player'} // todo: fix logic to add 'player--winner' when game ends
-    >
-      {children}
-    </section>
-  );
+function Player({ isActive, hasWon, children }) {
+  const active = isActive ? 'player--active player' : 'player';
+  const hasPlayerWon = hasWon ? 'player--winner player' : 'player';
+  return <section className={`${active} ${hasPlayerWon}`}>{children}</section>;
 }
 
-function Name({ children }) {
-  return <h2 className="name">{children}</h2>;
+function Name({ num }) {
+  return <h2 className="name">Player {num}</h2>;
 }
 
 function Score({ score }) {
   return <p className="score">{score}</p>;
 }
 
-function CurrScoreBox({ children }) {
+function CurrScoreWrapper({ children }) {
   return <div className="current">{children}</div>;
 }
 
@@ -171,7 +176,7 @@ function CurrScore({ currScore }) {
 function DiceImage({ src, className }) {
   return (
     <img
-      src={src}
+      src={`./src/assets/dice-${src}.png`}
       alt="playing dice"
       className={className}
     />
