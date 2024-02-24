5335738e6e182cd8d85aee289d89cf2fced493ee
kimhnh <kimhnh@gmail.com>
2024-02-24 23:50:29 +0900

    file structure update

---
+++ modal/src/data.jsx
@@ -0,0 +1,23 @@
+export const modalData = [
+  {
+    dataID: 1,
+    tab: "Marcus Tullius Cicero",
+    title: "Who created Lorem Ipsum?",
+    description:
+      "It is widely believed that the history of Lorem Ipsum originates with Cicero in the 1st Century BC and his text De Finibus bonorum et malorum. This philosophical work, also known as On the Ends of Good and Evil, was split into five books.",
+  },
+  {
+    dataID: 2,
+    tab: "Translation",
+    title: "What does it mean?",
+    description:
+      '"Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure".',
+  },
+  {
+    dataID: 3,
+    tab: "Usage",
+    title: "Lorem Ipsum",
+    description:
+      "Advancing to the 1960s, Lorem Ipsum was made popular by typeface manufacturer Letraset, who used it in their advertising campaigns. Letraset offered pages of Lorem Ipsum as rub down transfer sheets, which were widely used in the pre-computer era for layouts. These transfer pages, known as Letraset Body Type, were featured in the company's advertising and their popular catalogue.",
+  },
+];
