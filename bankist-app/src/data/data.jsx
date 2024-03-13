73f84512bce9539d5a31ffded2097f1c7e0fab74
kimhnh <kimhnh@gmail.com>
2024-03-14 00:16:21 +0900

    feat movement dates

--- bankist-app/src/data/data.jsx
+++ bankist-app/src/data/data.jsx
@@ -1,6 +1,102 @@
 export const initialData = [
   {
     owner: 'Jonas Schmedtmann',
+    movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+
+    movementsDates: [
+      '2019-11-18T21:31:17.178Z',
+      '2019-12-23T07:42:02.383Z',
+      '2020-01-28T09:15:04.904Z',
+      '2020-04-01T10:17:24.185Z',
+      '2023-05-08T14:11:59.604Z',
+      '2024-02-29T17:01:17.194Z',
+      '2024-03-03T23:36:17.929Z',
+      '2024-03-06T10:51:36.790Z',
+    ],
+    currency: 'EUR',
+    locale: 'pt-PT', // de-DE
+  },
+  {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+
+    movementsDates: [
+      '2019-11-01T13:15:33.035Z',
+      '2019-11-30T09:48:16.867Z',
+      '2020-12-25T06:04:23.907Z',
+      '2022-01-25T14:18:46.235Z',
+      '2023-02-05T16:33:06.386Z',
+      '2024-02-20T14:43:26.374Z',
+      '2024-03-10T18:49:59.371Z',
+      '2024-03-13T12:01:20.894Z',
+    ],
+    currency: 'USD',
+    locale: 'en-US',
+  },
+  {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+
+    movementsDates: [
+      '2019-11-01T13:15:33.035Z',
+      '2019-11-30T09:48:16.867Z',
+      '2019-12-25T06:04:23.907Z',
+      '2020-01-25T14:18:46.235Z',
+      '2020-02-05T16:33:06.386Z',
+      '2020-04-10T14:43:26.374Z',
+      '2020-06-25T18:49:59.371Z',
+      '2020-07-26T12:01:20.894Z',
+    ],
+    currency: 'CNY',
+    locale: 'zh-CN',
+  },
+  {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+
+    movementsDates: [
+      '2019-11-01T13:15:33.035Z',
+      '2019-11-30T09:48:16.867Z',
+      '2019-12-25T06:04:23.907Z',
+      '2020-01-25T14:18:46.235Z',
+      '2020-02-05T16:33:06.386Z',
+      '2020-04-10T14:43:26.374Z',
+      '2020-06-25T18:49:59.371Z',
+      '2020-07-26T12:01:20.894Z',
+    ],
+    currency: 'GBP',
+    locale: 'en-GB',
+  },
+];
+
+// create accountNo, balance, username properties (forEach OK for now...?)
+const createUsername = function (arr) {
+  return arr.forEach((el, index) => {
+    el.accountNo = index + 1;
+    el.balance = el.movements.reduce((acc, curr) => acc + curr, 0);
+    el.username = el.owner
+      .toLowerCase()
+      .split(' ')
+      .map((el) => el[0])
+      .join('');
+  });
+};
+
+createUsername(initialData);
+
+/* ORIGINAL DATA:
+
+[
+  {
+    owner: 'Jonas Schmedtmann',
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
@@ -25,17 +121,5 @@ export const initialData = [
   },
 ];
 
-// create accountNo, balance, username properties (forEach OK for now...?)
-const createUsername = function (arr) {
-  return arr.forEach((el, index) => {
-    el.accountNo = index + 1;
-    el.balance = el.movements.reduce((acc, curr) => acc + curr, 0);
-    el.username = el.owner
-      .toLowerCase()
-      .split(' ')
-      .map((el) => el[0])
-      .join('');
-  });
-};
 
-createUsername(initialData);
+*/
