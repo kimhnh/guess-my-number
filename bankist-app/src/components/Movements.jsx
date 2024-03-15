e6d4aa497275e413c6028e83b2f00b0005e8d7cd
kimhnh <kimhnh@gmail.com>
2024-03-15 23:09:05 +0900

    fix movement dates formatting

--- bankist-app/src/components/Movements.jsx
+++ bankist-app/src/components/Movements.jsx
@@ -1,28 +1,25 @@
 import formatCurrency from '../utils/formatCurrency';
 import Inner from './Inner';
 
-export default function Movements({ loggedUser, currMovs }) {
-  // format dates
-  function formatMovDates(account, index) {
-    // calculate days passed from NOW to movement date
-    function calcDates(date1, date2) {
-      return Math.round(Math.abs(date1 - date2));
-    }
-
-    const daysPassed = calcDates(new Date(), new Date(account.movementsDates[index]));
-
-    // conditionally return value [FIX]
-    if (daysPassed === 0) return 'Today';
-    if (daysPassed === 1) return 'Yesterday';
-    if (daysPassed < 8) {
-      return `${daysPassed} days ago`;
-    } else {
-      return new Intl.DateTimeFormat(account.locale).format(
-        new Date(account.movementsDates[index])
-      );
-    }
+function formatMovDates(account, date) {
+  // calculate days passed from NOW to movement date
+  function calcDates(date1, date2) {
+    return Math.round(Math.abs(date1 - date2) / (1000 * 60 * 60 * 24));
   }
 
+  const daysPassed = calcDates(new Date(), new Date(date)); // returns a rounded value
+
+  if (daysPassed == 0) return 'Today';
+  if (daysPassed == 1) return 'Yesterday';
+  if (daysPassed <= 7) {
+    return `${daysPassed} days ago`;
+  } else {
+    return new Intl.DateTimeFormat(account.locale).format(new Date(date)); // need new Date(date)
+  }
+}
+
+// currUser is a derived state
+export default function Movements({ currUser, currMovs }) {
   return (
     <div className='movements'>
       {currMovs.toReversed().map((mov, index, arr) => (
@@ -33,8 +30,10 @@ export default function Movements({ loggedUser, currMovs }) {
           <div className={`movements__type movements__type--${mov > 0 ? 'deposit' : 'withdrawal'}`}>
             {arr.length - index} {mov > 0 ? 'deposit' : 'withdrawal'}
           </div>
-          <div className='movements__date'>{formatMovDates(loggedUser, index)}</div>
-          <div className='movements__value'>{formatCurrency(loggedUser, mov)}</div>
+          <div className='movements__date'>
+            {formatMovDates(currUser, currUser.movementsDates[arr.length - 1 - index])}
+          </div>
+          <div className='movements__value'>{formatCurrency(currUser, mov)}</div>
         </Inner>
       ))}
     </div>
