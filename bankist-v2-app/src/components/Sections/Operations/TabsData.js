c503fe89d82493d99e6615de97e26bdf30f6a7d1
kimhnh <kimhnh@gmail.com>
2024-04-23 15:01:03 +0900

    chore init data

---
+++ bankist-v2-app/src/components/Sections/Operations/TabsData.js
@@ -0,0 +1,26 @@
+export const tabsData = [
+  {
+    num: 1,
+    button: 'Instant Transfers',
+    header: 'Transfer money to anyone, instantly! No fees, no BS.',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    svg: '/icons.svg#icon-upload',
+  },
+  {
+    num: 2,
+    button: 'Instant Loans',
+    header: 'Buy a home or make your dreams come true, with instant loans.',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    svg: '/icons.svg#icon-home',
+  },
+  {
+    num: 3,
+    button: 'Instant Closing',
+    header: 'No longer need your account? No problem! Close it instantly.',
+    description:
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    svg: '/icons.svg#icon-user-x',
+  },
+];
---
+++ bankist-v2-app/src/components/Sections/Testimonials/TestimonialsData.js
@@ -0,0 +1,29 @@
+export const testimonialsData = [
+  {
+    num: 1,
+    header: 'Best financial decision ever!',
+    quote:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quas quisquam non? Quas voluptate nulla minima deleniti optio ullam nesciunt, numquam corporis et asperiores laboriosam sunt, praesentium suscipit blanditiis. Necessitatibus id alias reiciendis, perferendis facere pariatur dolore veniam autem esse non voluptatem saepe provident nihil molestiae.',
+    img: './src/assets/images/user-1.jpg',
+    name: 'Aarav Lynn',
+    location: 'San Francisco, USA',
+  },
+  {
+    num: 2,
+    header: 'The last step to becoming a complete minimalist',
+    quote:
+      'Quisquam itaque deserunt ullam, quia ea repellendus provident, ducimus neque ipsam modi voluptatibus doloremque, corrupti laborum. Incidunt numquam perferendis veritatis neque repellendus. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo deserunt exercitationem deleniti.',
+    img: './src/assets/images/user-2.jpg',
+    name: 'Miyah Miles',
+    location: 'London, UK',
+  },
+  {
+    num: 3,
+    header: 'Finally free from old-school banks',
+    quote:
+      'Debitis, nihil sit minus suscipit magni aperiam vel tenetur incidunt commodi architecto numquam omnis nulla autem, necessitatibus blanditiis modi similique quidem. Odio aliquam culpa dicta beatae quod maiores ipsa minus consequatur error sunt, deleniti saepe aliquid quos inventore sequi. Necessitatibus id alias reiciendis, perferendis facere.',
+    img: './src/assets/images/user-3.jpg',
+    name: 'Francisco Gomes',
+    location: 'Lisbon, Portugal',
+  },
+];
