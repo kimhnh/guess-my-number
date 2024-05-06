d99a133854742aadc3070ef33088e03de9b6259c
kimhnh <kimhnh@gmail.com>
2024-05-06 18:56:36 +0900

    add data for features

---
+++ bankist-v2-app/src/components/Sections/Features/FeaturesData.js
@@ -0,0 +1,50 @@
+export const FeaturesData = [
+  {
+    type: 'image',
+    src: './src/assets/images/digital-lazy.jpg',
+    dataSrc: './src/assets/images/digital.jpg',
+    alt: 'computer',
+    visible: false,
+    num: 1,
+  },
+  {
+    type: 'text',
+    xlinkHref: '/icons.svg#icon-monitor',
+    header: '100% digital bank',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde alias sint quos? Accusantium a fugiat porro reiciendis saepe quibusdam debitis ducimus.',
+    num: 2,
+  },
+  {
+    type: 'text',
+    xlinkHref: '/icons.svg#icon-trending-up',
+    header: 'Watch your money grow',
+    description:
+      'Nesciunt quos autem dolorum voluptates cum dolores dicta fuga inventore ab? Nulla incidunt eius numquam sequi iste pariatur quibusdam!',
+    num: 3,
+  },
+  {
+    type: 'image',
+    src: './src/assets/images/grow-lazy.jpg',
+    dataSrc: './src/assets/images/grow.jpg',
+    alt: 'plant',
+    visible: false,
+    num: 4,
+  },
+  {
+    type: 'image',
+    src: './src/assets/images/card-lazy.jpg',
+    dataSrc: './src/assets/images/card.jpg',
+    alt: 'credit card',
+    visible: false,
+    num: 5,
+  },
+  {
+    type: 'text',
+    xlinkHref: '/icons.svg#icon-credit-card',
+    header: 'Free debit card included',
+    description:
+      'Quasi, fugit in cumque cupiditate reprehenderit debitis animi enim eveniet consequatur odit quam quos possimus assumenda dicta fuga inventore ab.',
+    num: 6,
+  },
+];
